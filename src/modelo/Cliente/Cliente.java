fa49152330171ba5a87e555890a6c8f332333933
unknown <eduardoscunico@gmail.com>
2024-09-04 11:27:25 -0300

    Criação
    Classe cliente criada

---
+++ src/modelo/Cliente/Cliente.java
@@ -0,0 +1,5 @@
+package modelo.Cliente;
+
+public class Cliente {
+
+}
---
+++ src/modelo/entidade/Cliente/Cliente.java
@@ -0,0 +1,14 @@
+package modelo.entidade.Cliente;
+
+public class Cliente {
+	String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
