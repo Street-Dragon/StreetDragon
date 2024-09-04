84be2877d38e3d7ee205f304a61ecbd6e84e5a52
Carlos Furtunato <Carlos.esf@aluno.ifsc.edu.br>
2024-09-04 11:53:56 -0300

    Atualizada Classe Contato

---
+++ src/modelo/entidade/contato/Contato.java
@@ -0,0 +1,20 @@
+package modelo.entidade.contato;
+
+public class Contato {
+	private  String email;
+	private Long telefone;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	} 
+
+}
