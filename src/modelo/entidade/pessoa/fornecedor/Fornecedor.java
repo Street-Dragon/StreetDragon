c9d5323b760ea78a41382647e5c13a2aa718f032
Carlos Furtunato <Carlos.esf@aluno.ifsc.edu.br>
2024-09-04 11:52:59 -0300

    Atualizado Classe Fornecedor

---
+++ src/modelo/entidade/pessoa/fornecedor/Fornecedor.java
@@ -0,0 +1,36 @@
+package modelo.entidade.pessoa.fornecedor;
+
+public class Fornecedor {
+	
+	private Integer cpnj;
+	private Integer cep;
+	private String rua;
+	private Integer numero;
+	public Integer getCpnj() {
+		return cpnj;
+	}
+	public void setCpnj(Integer cpnj) {
+		this.cpnj = cpnj;
+	}
+	public Integer getCep() {
+		return cep;
+	}
+	public void setCep(Integer cep) {
+		this.cep = cep;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	
+	
+
+}
