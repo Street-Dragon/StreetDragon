de15bd3af88663033b365cc11ad9d8250ca0d395
Carlos Furtunato <Carlos.esf@aluno.ifsc.edu.br>
2024-09-04 11:56:33 -0300

    Criada Classe Pessoa Abstrata

---
+++ src/modelo/entidade/pessoa/fornecedor/Pessoa.java
@@ -0,0 +1,29 @@
+package modelo.entidade.pessoa.fornecedor;
+
+import modelo.entidade.contato.Contato;
+
+abstract class Pessoa {
+	private Integer id;
+	private String nome;
+	private Contato contato;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Contato getContato() {
+		return contato;
+	}
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+	
+	
+}
