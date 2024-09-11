96fb3dd279f8b62e693431c25939dc97fbd0e6fa
Carlos Eduardo de Souza Furtunato <carlos.esf@aluno.ifsc.edu.br>
2024-09-11 11:08:21 -0300

    Criada de fato a Classe Pessoa

--- src/modelo/entidade/item/Item.java
+++ src/modelo/entidade/item/Item.java
@@ -1,5 +1,27 @@
 package modelo.entidade.item;
 
 public class Item {
+	
+	private Produto produo;
+	private int quantidade;
+	private Promocao promocao;
+	public Produto getProduo() {
+		return produo;
+	}
+	public void setProduo(Produto produo) {
+		this.produo = produo;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public Promocao getPromocao() {
+		return promocao;
+	}
+	public void setPromocao(Promocao promocao) {
+		this.promocao = promocao;
+	} 
 
 }
