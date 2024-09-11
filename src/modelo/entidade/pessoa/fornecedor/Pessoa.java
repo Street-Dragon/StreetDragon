867d259c267736e955c922ec32de985eb35d169a
Carlos Eduardo de Souza Furtunato <carlos.esf@aluno.ifsc.edu.br>
2024-09-11 11:03:46 -0300

    Corrigido classe Pessoa

--- src/modelo/entidade/pessoa/fornecedor/Pessoa.java
+++ src/modelo/entidade/pessoa/fornecedor/Pessoa.java
@@ -3,13 +3,13 @@ package modelo.entidade.pessoa.fornecedor;
 import modelo.entidade.contato.Contato;
 
 abstract class Pessoa {
-	private Integer id;
+	private int id;
 	private String nome;
 	private Contato contato;
-	public Integer getId() {
+	public int getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 	public String getNome() {
