039fc53e549a03544791e80d5e456cc63152713f
Douglas E. K. <douglas.k2005@aluno.ifsc.edu.br>
2024-10-22 12:09:22 -0300

    Cadstrar duplicado
    corrigido

--- src/controle/entidade/funcionariocontrole/FuncionarioControle.java
+++ src/controle/entidade/funcionariocontrole/FuncionarioControle.java
@@ -97,42 +97,7 @@ public class FuncionarioControle {
 	
 	
 
-	private void cadastrarFuncionario() {
-		String nome = cadastroFuncionario.getTextNome();
-		char[] senhaArray = cadastroFuncionario.getPasswordField();
-		String cpf = cadastroFuncionario.getTextCpf();
-		boolean isAdm = cadastroFuncionario.getChckbxAdm();
-		String email = cadastroFuncionario.getTextEmail();
-		String senha = new String(senhaArray);
-		String telefone = cadastroFuncionario.getTextTelefone();
 
-		if (nome.isEmpty() || cpf.isEmpty() || senha.isEmpty()) {
-			JOptionPane.showMessageDialog(cadastroFuncionario, "Preencha todos os campos obrigatórios.", "Erro",
-					JOptionPane.ERROR_MESSAGE);
-			return;
-		}
-
-		Funcionario funcionario = new Funcionario();
-		funcionario.setNome(nome);
-		funcionario.setCpf(cpf);
-		funcionario.setSenhaFuncionario(senha);
-		funcionario.setAdm(isAdm);
-
-		Contato contato = new Contato();
-		contato.setEmail(email);
-		contato.setTelefone(telefone);
-		funcionario.setContato(contato);
-
-		if (funcionarioDAO.cadastrarFuncionario(funcionario)) {
-			JOptionPane.showMessageDialog(cadastroFuncionario, "Funcionário cadastrado com sucesso!", "Sucesso",
-					JOptionPane.INFORMATION_MESSAGE);
-
-		} else {
-			JOptionPane.showMessageDialog(cadastroFuncionario, "Erro ao cadastrar funcionário.", "Erro",
-					JOptionPane.ERROR_MESSAGE);
-		}
-		return;
-	}
     
     private void cadastrarFuncionario() {
         String nome = cadastroFuncionario.getTextNome();
