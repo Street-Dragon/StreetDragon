d32041e7f2bc71133121c2101bf67935830eb851
unknown <mariana.cb@aluno.ifsc.edu.br>
2024-11-05 10:36:15 -0300

    Criadas constantes de cores

---
+++ src/utils/Cores.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.awt.Color;
+
+public class Cores {
+
+	public static final Color COR_AZUL = new Color(114, 148, 235);
+	public static final Color COR_ROSA_CLARO = new Color(246, 233, 233);
+	public static final Color COR_ROSA = new Color(250, 187, 187);
+	public static final Color COR_ROSA_ESCURO = new Color(255, 149, 149);
+	public static final Color COR_VERMELHO = new Color(226, 61, 40);
+
+}
