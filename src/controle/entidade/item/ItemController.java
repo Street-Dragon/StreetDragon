b882158209127a613c1965a69ab0c9aa5ae2b6d8
Douglas E. K <douglas.k2005@aluno.ifsc.edu.br>
2024-11-26 09:45:08 -0300

    push
    push

--- src/controle/entidade/item/ItemController.java
+++ src/controle/entidade/item/ItemController.java
@@ -10,19 +10,21 @@ import visao.TelaVenda;
 
 public class ItemController { 
 	private TelaVenda view; 
-	private ItemDAO itemDAO;
+	private ItemDAO itemDAO = new ItemDAO();
 	
 //	public ItemController(VendaView view) {
 //		this.view = view;
 //		this.itemDAO = new ItemDAO(); 
-//		this.view.addAdicionarButtonListener(new AdicionarButtonListener()); 
-//		} class AdicionarButtonListener implements ActionListener {
+//		view.addAdicionarButtonListener(new AdicionarButtonListener()); 
+//		} 
+//	
+//	class AdicionarButtonListener implements ActionListener {
 //			@Override public void actionPerformed(ActionEvent e) {
 //				Item item = new Item();
 //				item.setQuantidade(Integer.parseInt(view.getQuantidadeField())); 
 //				item.setIdItem(Integer.parseInt(view.getIdItemField()));
 //				item.setIdProduto(Integer.parseInt(view.getIdProdutoField())); 
-//				itemDAO.adicionarItem(item); view.atualizarTabela(); 
-//				}
-//			} 
-//		}
+////				itemDAO.adicionarItem(item); view.atualizarTabela(); 
+//			}
+//		} 
+}
