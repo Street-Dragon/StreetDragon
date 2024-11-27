e5ef36940588ad4b86beb8d5cddb0b9ca9132676
marianaacb <mariana.cb@aluno.ifsc.edu.br>
2024-11-27 11:32:09 -0300

    algumas correções no itemDAO

--- src/modelo/dao/item/ItemDAO.java
+++ src/modelo/dao/item/ItemDAO.java
@@ -1,4 +1,5 @@
 package modelo.dao.item;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,32 +11,33 @@ import java.util.List;
 import controle.entidade.conexao.ConexaoBD;
 import modelo.entidade.item.Item;
 import modelo.entidade.produto.Produto;
+
 public class ItemDAO {
-	
-    public boolean cadastrarItem(Item item) {
-        
-        int produtoId = verificarProdutoExistente(item.getProduto().getNomeProduto());
-        
-        if (produtoId == -1) {
-            return false; 
-        }
 
-        String sqlItem = "INSERT INTO item (quantidade, produto_id) VALUES (?, ?)";
+	public boolean cadastrarItem(Item item) {
 
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmtItem = conn.prepareStatement(sqlItem)) {
+		int produtoId = verificarProdutoExistente(item.getProduto().getNomeProduto());
 
-            stmtItem.setInt(1, item.getQuantidade());
-            stmtItem.setInt(2, produtoId); 
-            stmtItem.executeUpdate();
-            return true;
+		if (produtoId == -1) {
+			return false;
+		}
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    
+		String sqlItem = "INSERT INTO item (quantidade, produto_id) VALUES (?, ?)";
+
+		try (Connection conn = ConexaoBD.getConexaoMySQL();
+				PreparedStatement stmtItem = conn.prepareStatement(sqlItem)) {
+
+			stmtItem.setInt(1, item.getQuantidade());
+			stmtItem.setInt(2, produtoId);
+			stmtItem.executeUpdate();
+			return true;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
 //    public Produto buscarProdutoPorNome(String nomeProduto) {
 //        Produto produto = null;
 //        String sqlProduto = "SELECT id, nome, valor, material, categoria, variação, estoque, tamanho   FROM produto WHERE nome = ?";
@@ -65,75 +67,70 @@ public class ItemDAO {
 //        return produto; // Retorna o Produto encontrado ou null se não encontrado
 //    }
 
-    
-    private int verificarProdutoExistente(String nomeProduto) {
-        String sqlProduto = "SELECT id FROM produto WHERE nome = ?";
+	private int verificarProdutoExistente(String nomeProduto) {
+		String sqlProduto = "SELECT idProduto FROM produto WHERE nome = ?";
 
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sqlProduto)) {
+		try (Connection conn = ConexaoBD.getConexaoMySQL();
+				PreparedStatement stmt = conn.prepareStatement(sqlProduto)) {
 
-            stmt.setString(1, nomeProduto);
-            ResultSet rs = stmt.executeQuery();
+			stmt.setString(1, nomeProduto);
+			ResultSet rs = stmt.executeQuery();
 
-            if (rs.next()) {
-                return rs.getInt("id"); 
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+			if (rs.next()) {
+				return rs.getInt("idProduto");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-        return -1; 
-    }
+		return -1;
+	}
 
-    
-    public boolean excluirItem(int idItem) {
-    	   String sql = "DELETE FROM item WHERE id_item = ?";
-        
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+	public boolean excluirItem(int idItem) {
+		String sql = "DELETE FROM item WHERE id_item = ?";
 
-        	stmt.setInt(1, idItem);
-            int rowsAffected = stmt.executeUpdate();
-            return rowsAffected > 0;
+		try (Connection conn = ConexaoBD.getConexaoMySQL(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    
-    public List<Item> listarItens() {
-        List<Item> itens = new ArrayList<>();
-        String sql = "SELECT i.id, i.quantidade, p.nome, p.preco, p.id FROM item i " +
-                "JOIN produto p ON i.produto_id = p.id";
+			stmt.setInt(1, idItem);
+			int rowsAffected = stmt.executeUpdate();
+			return rowsAffected > 0;
 
-        try (Connection conn = ConexaoBD.getConexaoMySQL();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
-            ResultSet rs = stmt.executeQuery();
+	public List<Item> listarItens() {
+		List<Item> itens = new ArrayList<>();
+		String sql = "SELECT i.id AS item_id, i.quantidade, p.nome, p.preco, p.id AS produto_id " + "FROM item i "
+				+ "JOIN produto p ON i.produto_id = p.id";
 
-            while (rs.next()) {
-                Produto produto = new Produto();
-                produto.setIdProduto(rs.getInt("idProduto"));
-                produto.setNomeProduto(rs.getString("p.nome"));
-                produto.setValor(rs.getFloat("p.valor"));
-                produto.setMaterial(rs.getString("p.material"));
-                produto.setCategoria(rs.getString("p.categoria"));
-                produto.setVariacao(rs.getString("p.variação"));
-                produto.setQuantEstoque(rs.getInt("p.estoque"));
-                produto.setTamanho(rs.getString("p.tamanho"));
-                
+		try (Connection conn = ConexaoBD.getConexaoMySQL(); PreparedStatement stmt = conn.prepareStatement(sql)) {
 
-                Item item = new Item();
-                item.setQuantidade(rs.getInt("i.quantidade"));
-                item.setProduto(produto);
+			ResultSet rs = stmt.executeQuery();
 
-                itens.add(item);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return itens;
-    }
-    
+			while (rs.next()) {
+				Produto produto = new Produto();
+				produto.setIdProduto(rs.getInt("produto_id"));
+				produto.setNomeProduto(rs.getString("p.nome"));
+				produto.setValor(rs.getFloat("p.preco"));
+				produto.setMaterial(rs.getString("p.material"));
+				produto.setCategoria(rs.getString("p.categoria"));
+				produto.setVariacao(rs.getString("p.variação"));
+				produto.setQuantEstoque(rs.getInt("p.estoque"));
+				produto.setTamanho(rs.getString("p.tamanho"));
+
+				Item item = new Item();
+				item.setQuantidade(rs.getInt("i.quantidade"));
+				item.setProduto(produto);
+
+				itens.add(item);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return itens;
+	}
+
 }
